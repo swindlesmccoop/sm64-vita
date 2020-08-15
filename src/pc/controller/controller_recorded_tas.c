39610a91f4a7276894fd564f4d6aa5f66c165424
bythos14 <richiehyacinth@gmail.com>
2020-08-15 19:19:19 -0400

    Make TAS an option eventually

--- src/pc/controller/controller_recorded_tas.c
+++ src/pc/controller/controller_recorded_tas.c
@@ -6,7 +6,11 @@
 static FILE *fp;
 
 static void tas_init(void) {
+    #ifdef TARGET_VITA
+    fp = fopen("ux0:data/cont.m64", "rb");
+    #else
     fp = fopen("cont.m64", "rb");
+    #endif
     if (fp != NULL) {
         uint8_t buf[0x400];
         fread(buf, 1, sizeof(buf), fp);
